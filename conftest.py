8a288f43b7e5ea26c68cfa0b586eb1e679586930
Dewr01 <pivont@mail.ru>
2025-08-07 10:30:23 +0300

    PyTest — параметризация, конфигурирование, плагины

--- conftest.py
+++ conftest.py
@@ -2,14 +2,23 @@ import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 
+def pytest_addoption(parser):
+    parser.addoption("--language", action="store", default="en",
+                     help="Choose language: es, fr, ru, etc.")
+
+
 @pytest.fixture(scope="function")
-def browser():
+def browser(request):
     print("\nstart browser for test..")
+    language = request.config.getoption("language")
     service = Service(ChromeDriverManager().install())
-    browser = webdriver.Chrome(service=service)
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(service=service, options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
